1817d4fd0d7f76bfee83c6cbfb38aa8939744d4c
mert-kurttutan <kurttutan.mert@gmail.com>
2026-09-10 12:20:59 +0300

    Use NixOS zstd channel tarball

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Development environment for the benchmark repository playground";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
